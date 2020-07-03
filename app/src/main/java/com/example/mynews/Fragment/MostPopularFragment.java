32c0df8ce23762b10be3dea4883b6bae50522ef0
sarahdasilva <sarahtlse@hotmail.fr>
2020-07-03 16:06:53 +0200

    integration ViewModel in the fragmentMostPopular in progress

--- app/src/main/java/com/example/mynews/Fragment/MostPopularFragment.java
+++ app/src/main/java/com/example/mynews/Fragment/MostPopularFragment.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,6 +26,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,9 +36,9 @@ public class MostPopularFragment extends Fragment {
 
 
     private static final String JSON_URL = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=lvzzkPeHJEIDxfpTaqSb3Azu9LDnO4Fv";
-    private List<ItemByArticle> itemByArticle;
 
     private RequestQueue mQueue;
+    private List<ItemByArticle> itemByArticle;
     private MostPopularViewModel mMostPopularViewModel;
 
     public MostPopularFragment() {
@@ -42,57 +46,66 @@ public class MostPopularFragment extends Fragment {
 
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        itemByArticle = new ArrayList<>();
+        mMostPopularViewModel = new MostPopularViewModel();
+
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+        public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState){
 
-        this.requestApi();
+            this.requestApi();
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_most_popular, container, false);
+        View root = inflater.inflate(R.layout.fragment_most_popular, container, false);
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_most_popular, container, false);
-
-
-    }
-
-    private void requestApi() {
-
-        this.mQueue = Volley.newRequestQueue(this.getContext());
-        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONObject>() {
-
+        //LiveData Observer
+        mMostPopularViewModel.getList().observe(getViewLifecycleOwner(), new Observer<List<ItemByArticle>>() {
             @Override
-            public void onResponse(final JSONObject response) {
-                try {
-
-                    final JSONArray newsArray = response.getJSONArray("results");
-
-                    for (int i = 0; i < newsArray.length(); i++) {
-                        final JSONObject newObjet = newsArray.getJSONObject(i);
-                        final String sectionObject = newObjet.getString("section");
-                        final JSONArray mediaArray = newObjet.getJSONArray("multimedia");
-                        final JSONObject mediaObject = mediaArray.getJSONObject(0);
-
-
-                    }
-
-                } catch (final JSONException e) {
-                    e.printStackTrace();
-                }
-
+            public void onChanged(List<ItemByArticle> itemByArticles) {
 
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(final VolleyError error) {
-                error.printStackTrace();
-                Toast.makeText(MostPopularFragment.this.getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
+        } );{
+
         });
 
-        this.mQueue.start();
-
-        this.mQueue.add(request);
-
+        return root;
     }
-}
+
+
+        private void requestApi () {
+            this.mQueue = Volley.newRequestQueue(this.getContext());
+            final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(final JSONObject response) {
+                    try {
+                        final JSONArray newsArray = response.getJSONArray("results");
+
+                        for (int i = 0; i < newsArray.length(); i++) {
+                            final JSONObject newObjet = newsArray.getJSONObject(i);
+                            final String sectionObject = newObjet.getString("section");
+                            final JSONArray mediaArray = newObjet.getJSONArray("multimedia");
+                            final JSONObject mediaObject = mediaArray.getJSONObject(0);
+                        }
+                        mMostPopularViewModel.setItemByArticle(itemByArticle);
+                    } catch (final JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    error.printStackTrace();
+                    Toast.makeText(MostPopularFragment.this.getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            this.mQueue.start();
+            this.mQueue.add(request);
+        }
+    }
 
