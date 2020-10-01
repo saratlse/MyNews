27216c0c5d5b7606dc7c616a760d26fca47d7dd3
sarahdasilva <sarahtlse@hotmail.fr>
2020-10-01 12:57:36 +0200

    DatePicker done

--- app/src/main/java/com/example/mynews/Controllers/SearchArticlesActivity.java
+++ app/src/main/java/com/example/mynews/Controllers/SearchArticlesActivity.java
@@ -11,9 +11,11 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.View;
@@ -22,19 +24,21 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.example.mynews.Fragment.DatePickerFragment;
 import com.example.mynews.R;
 import com.example.mynews.View.SearchArticlesViewModel;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SearchArticlesActivity extends AppCompatActivity implements View.OnClickListener {
+public class SearchArticlesActivity extends AppCompatActivity implements DatePickerFragment.OnDateSetListener {
 
     //  Adding @BindView in order to indicate to ButterKnife to get & serialise it
     //@BindView(R.id.menu) Menu mMenu;
@@ -43,9 +47,9 @@ public class SearchArticlesActivity extends AppCompatActivity implements View.On
     @BindView(R.id.search_query_term_editText)
     EditText mSearchQueryTerm;
     @BindView(R.id.search_begin_date)
-    EditText mSearchBeginDate;
-    @BindView(R.id.search_end_date)
-    EditText mSearchEndDate;
+   EditText mSearchBeginDate;
+   @BindView(R.id.search_end_date)
+   EditText mSearchEndDate;
     //  @BindView(R.id.checkbox_container) CheckBox mCheckBox;
     @BindView(R.id.search_articles_arts)
     CheckBox mCheckBoxArts;
@@ -57,20 +61,27 @@ public class SearchArticlesActivity extends AppCompatActivity implements View.On
     Button mButton;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_search_articles);
         ButterKnife.bind(this);
 
+
+        DialogFragment datePicker = new DatePickerFragment();
+
         mSearchBeginDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus){
-            //This method calls show() on a new instance of the DialogFragment defined above.
-            // The show() method requires an instance of FragmentManager and a unique tag name for the fragment.
-                if (v == mSearchBeginDate && hasFocus){
-                    DialogFragment newFragment = new DialogFragment();
-                    newFragment.show(getSupportFragmentManager(),"datePicker");
+            public void onFocusChange(View v, boolean hasFocus) {
+                //This method calls show() on a new instance of the DialogFragment defined above.
+                // The show() method requires an instance of FragmentManager and a unique tag name for the fragment.
+                if (hasFocus) {
+                    datePicker.show(getSupportFragmentManager(), "beginDatePicker");
+                } else {
+                    datePicker.dismiss();
                 }
 
             }
@@ -78,22 +89,47 @@ public class SearchArticlesActivity extends AppCompatActivity implements View.On
         mSearchEndDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if (v == mSearchEndDate && hasFocus){
-                    DialogFragment newFragment = new DialogFragment();
-                    newFragment.show(getSupportFragmentManager(),"datePicker");
+                if (hasFocus) {
+                    datePicker.show(getSupportFragmentManager(), "endDatePicker");
+                } else {
+                    datePicker.dismiss();
                 }
             }
         });
-
     }
+
+
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
-    public void onClick(View v) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        // display the date chosen by the user
+        Locale locale = getResources().getConfiguration().locale;
+        final Calendar calendar = Calendar.getInstance(locale);
+       // final DateFormat dateFormat = DateFormat.getDateFormat(DateFormat.MEDIUM, locale);
+        final String myFormat = "dd/MM/yy";
+        final SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
+        if (getCurrentFocus() != null) {
+            switch (getCurrentFocus().getId()) {
 
+                case R.id.search_begin_date:
+                    calendar.set(year, month, dayOfMonth);
+                    mSearchBeginDate.setText(sdf.format(calendar.getTime()));
+                    // mEditor.putString("beginDate", sdf.format(calendar.getTime()));
+                    break;
+                case R.id.search_end_date:
+                    calendar.set(year, month, dayOfMonth);
+                    mSearchEndDate.setText(sdf.format(calendar.getTime()));
+                    // mEditor.putString("endDate", sdf.format(calendar.getTime()));
+            }
+            //mEditor.commit();
+        }
     }
 
-    }
+
+}
+
+
+
--- app/src/main/java/com/example/mynews/Fragment/DatePickerFragment.java
+++ app/src/main/java/com/example/mynews/Fragment/DatePickerFragment.java
@@ -3,8 +3,12 @@ package com.example.mynews.Fragment;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
@@ -12,26 +16,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.mynews.R;
 
 import java.util.Calendar;
+import java.util.Locale;
+
 
 public  class DatePickerFragment extends DialogFragment
         implements DatePickerDialog.OnDateSetListener {
 
-    private OnDateSetListener callbackListener;
+
+
+
+    OnDateSetListener onDateSetListener;
+
 
     public interface OnDateSetListener {
         void onDateSet(DatePicker view, int year, int month, int dayOfMonth);
     }
 
-    @Override
+   @Override
     public void onAttach(Context activity) {
         super.onAttach(activity);
 
         try {
-            callbackListener = (OnDateSetListener) activity;
+            onDateSetListener = (OnDateSetListener) activity;
 
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString() + " must implement OnDateSetListener.");
@@ -42,20 +53,26 @@ public  class DatePickerFragment extends DialogFragment
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
-        final Calendar c = Calendar.getInstance();
+        final Calendar c = Calendar.getInstance(getResources().getConfiguration().locale);
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
+
 
         // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
 
-    public void onDateSet(DatePicker view, int year, int month, int day) {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-        if (callbackListener != null) {
-            callbackListener.onDateSet(view, year, month, day);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year,month,dayOfMonth);
+
+       if (onDateSetListener!= null) {
+           onDateSetListener.onDateSet(view, year, month, dayOfMonth);
     }
+    }
+}
 
-}}
