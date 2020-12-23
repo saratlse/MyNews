bcf71e57514bf9327b1d754f09d5909a0ee76d0d
sarahdasilva <sarahtlse@hotmail.fr>
2020-12-23 09:58:46 +0100

    NotificationsActivity in progress

---
+++ app/src/main/java/com/example/mynews/Controllers/NotificationsActivity.java
@@ -0,0 +1,28 @@
+package com.example.mynews.Controllers;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.mynews.R;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        
+    }
+}
